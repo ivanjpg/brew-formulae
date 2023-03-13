e99e5edabdbf6752e097e4fa2c53ad8dd8ded91b
Ivan Pompa-García <ivan.pompa@gmail.com>
2023-03-13 13:10:44 -0600

    Changed name for gracepdf formula

--- gracepdf.rb
+++ gracepdf.rb
@@ -1,4 +1,4 @@
-class Grace < Formula
+class GracePDF < Formula
   desc "WYSIWYG 2D plotting tool for X11 (with PDFLibLite 7"
   homepage "https://plasma-gate.weizmann.ac.il/Grace/"
   url "https://deb.debian.org/debian/pool/main/g/grace/grace_5.1.25.orig.tar.gz"
